30876cbd974a57c2572f7c54e29f2d783c836737
Renier Vargas <renier1989@gmail.com>
2024-01-13 17:44:41 -0500

    742. Probando la aplicación Completa

--- src/__tests__/App.js
+++ src/__tests__/App.js
@@ -1,3 +1,22 @@
-test('Primera prueba', () => { 
+import React from "react";
+import { fireEvent, render, screen } from "@testing-library/react";
 
- })
+import "@testing-library/jest-dom/extend-expect";
+import userEvent from "@testing-library/user-event";
+import App from "../App";
+
+test("<App/> Aplicacion Carga bien la primera vez", () => {
+    // const wrapper = render(<App/>);
+    // wrapper.debug();
+    render(<App />);
+
+    expect(screen.getByText('Administrador de Pacientes')).toBeInTheDocument();
+    expect(screen.getByTestId('nombre-app').textContent).toBe('Administrador de Pacientes');
+    expect(screen.getByTestId('nombre-app').tagName).toBe('H1');
+
+    expect(screen.getByText('Crear Cita')).toBeInTheDocument();
+    expect(screen.getByText('No hay citas')).toBeInTheDocument();
+    
+
+
+});
--- src/__tests__/Formulario.js
+++ src/__tests__/Formulario.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {fireEvent, getByTestId, render, screen} from '@testing-library/react'
+import {fireEvent, render, screen} from '@testing-library/react'
 import Formulario from '../components/Formulario'
 import '@testing-library/jest-dom/extend-expect'
 import userEvent from '@testing-library/user-event';
