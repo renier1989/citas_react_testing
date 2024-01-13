86b78327b22ec39add37cf2261250a10b15bf965
Renier Vargas <renier1989@gmail.com>
2024-01-13 16:57:36 -0500

    740. Probando Funciones, Submits de Formulario y Validacion

--- src/__tests__/Formulario.js
+++ src/__tests__/Formulario.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import {render, screen} from '@testing-library/react'
+import {fireEvent, render, screen} from '@testing-library/react'
 import Formulario from '../components/Formulario'
 import '@testing-library/jest-dom/extend-expect'
+
+const crearCita = jest.fn();
 
 test("<Formulario/> Carga del formulario y revisando funcionamiento", () => {
     //  // esto es para ver que el componente se monto de forma correcta 
@@ -13,8 +15,9 @@ test("<Formulario/> Carga del formulario y revisando funcionamiento", () => {
     // const {getByText} = render(<Formulario />)
     // expect( getByText('Crear Cita') ).toBeInTheDocument();
 
-    
-    render(<Formulario />)
+    // aqui el componente de Formulario espera una propiedad que es crearCita, se pueden crear funcion Spy con Jest. para simular esa funcionalidad 
+
+    render(<Formulario crearCita={crearCita} />)
 
     // Heading
     const title =  screen.getByTestId('titulo');
@@ -29,3 +32,22 @@ test("<Formulario/> Carga del formulario y revisando funcionamiento", () => {
     expect( screen.getByTestId('btn-submit').textContent).not.toBe('Agregar Nueva Cita');
 
 });
+
+
+test('<Formulario/> Validación del formulario', ()=>{
+
+    render(<Formulario crearCita={crearCita} />);
+
+    const buttonSubmit = screen.getByTestId('btn-submit');
+    fireEvent.click(buttonSubmit); // dispara un evento en el formulario que es el de dar un click
+
+    // reviso que al dar el click exista el mensaje de la alerta, asigno un identificador de prueba
+    const alerta = screen.getByTestId('alerta');
+    expect(alerta).toBeInTheDocument(); 
+    // aqui como previamente se simulo un click al formulario , se espera la alerta
+    expect(alerta.textContent).toBe('Todos los campos son obligatorios'); 
+    expect(alerta.tagName).toBe('P'); 
+    
+
+
+})
